720c0d5b0a4eb201fd2cb58e02ede3848716ae5a
MMK <staph103@gmail.com>
2023-12-05 00:18:30 -0500

    setting api url to variables and trying to get value from html input

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+/* 8b386b72e48c2a304f8f25a691112ef3 set to a variable APIKEY */
+//  + philadelphia + '&appid=8b386b72e48c2a304f8f25a691112ef3'
+var frontUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='
+var city = $('#citySearch').val();
+var endUrl = '&appid=8b386b72e48c2a304f8f25a691112ef3'
+var fullUrl = frontUrl + city + endUrl;
+
+ 
+
+ $('.btn').on("click",);
