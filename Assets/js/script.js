042d4a7742e8d8977a8814c5fc603b10bde34066
MMK <staph103@gmail.com>
2023-12-05 21:30:19 -0500

    trying to render javascript created elements on html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 /* 8b386b72e48c2a304f8f25a691112ef3 set to a variable APIKEY */
 //  + philadelphia + '&appid=8b386b72e48c2a304f8f25a691112ef3'
 var frontUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='
-var cityEl = $('#citySearch');
+var cityEl = $('.citySearch');
 var endUrl = '&appid=8b386b72e48c2a304f8f25a691112ef3&units=imperial'
 var searchBtn = $('.s-btn');
 var cardSlot = $('cardSlot')
@@ -9,14 +9,15 @@ var fIcon = 'https://openweathermap.org/img/wn/'
 
 
 
-// console.log(city);  
+  
+// function searchedCities(){
+// // var savedList = JSON.parse(localStorage.getItem("searches")) || [];
 
-
-
+// }
 
 function searchCity() {
 
-    let city = cityEl.val()
+    let city = cityEl.val();
     var fullUrl = frontUrl + city + endUrl;
     // let fullUrl = `${frontUrl+city+endUrl}`
     console.log(city);
@@ -30,54 +31,55 @@ function searchCity() {
             // Use the console to examine the response
             console.log(data);
             console.log(data.list.length);
+
+
             // TODO: Loop through the data and generate your HTML
-            for (var i = 0; i < data.list.length; i = i + 8 ) {
-                 //= document.createElement('h3');
-                 //= document.createElement('p');
+            for (var i = 0; i < data.list.length; i = i + 8) {
+                //= document.createElement('h3');
+                //= document.createElement('p');
                 var weatherDate = data.list[i].dt_txt;
                 var temp = data.list[i].main.temp;
                 var windInfo = data.list[i].wind.speed;
                 var humidity = data.list[i].main.humidity;
-                var iconCode =  data.list[i].weather[0].icon;
-                var weatherIcon = fIcon + iconCode + ".png";
+                var iconCode = data.list[i].weather[0].icon;
+                var weatherIcon = fIcon + iconCode + "@2x.png";
                 console.log(weatherIcon);
 
-                var cardInfo = $('<div>');
-                var weatherDateEl = $('<p>').text(weatherDate)
+                var cardInfo = $('<li>');
+                var weatherDateEl = $('<h4>').text(weatherDate)
                 var tempEl = $('<p>').text(temp)
                 var windInfoEl = $('<p>').text(windInfo)
                 var humidityEl = $('<p>').text(humidity)
                 var weatherIconEl = $('<img>');
-    
-                cardInfo.append(weatherDateEl)
-                cardInfo.append(tempEl)
-                cardInfo.append(windInfoEl)
-                cardInfo.append(humidityEl)
-                cardInfo.append(weatherIconEl)
-                // cardSlot.addClass('card row')
-                cardSlot.append(cardInfo)
-                
 
-                // userContainer.append(users);
-                // userContainer.append(url);
-                console.log(temp);
+                // cardSlot.addClass('card row')
+                cardSlot.append(cardInfo);
+
                 console.log(weatherDate);
+                console.log(temp);
                 console.log(windInfo);
                 console.log(humidity);
             }
+            while (cardSlot.firstChild) {
+                cardSlot.removeChild(cardSlot.firstChild)
+                cardInfo.append(weatherDateEl);
+                cardInfo.append(tempEl);
+                cardInfo.append(windInfoEl);
+                cardInfo.append(humidityEl);
+                cardInfo.append(weatherIconEl);
+                cardSlot.appendChild(cardInfo);
+            }
+
+            // °F
+
+
+
+
+        });
         
 
-        
 
-
-    // userContainer.append(users);
-    // userContainer.append(url);
-    // console.log(temp);
-    // console.log(weatherDate);
-
-
-
-    });
+       
     // get nearby values of the description in jQuery
     //  var text = $(this).siblings('.description').val();
     //  // get the id attribute of the parent div element
@@ -89,5 +91,3 @@ searchBtn.on('click', searchCity);
 
 
 
-// var weatherDate = data.list[0].dt;
-// Var temp = data.list[0].main.temp
